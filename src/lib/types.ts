aa4b5264a2fc722b170d784124b6ae06bb422e63
Łukasz Godlewski <lgodlewski@itsg.com.pl>
2025-11-05 13:46:30 +0100

    Initial commit

---
+++ src/lib/types.ts
@@ -0,0 +1,18 @@
+export type InvoiceStatus = 'OVERDUE' | 'DUE_TODAY' | 'DUE_IN' | 'PAID';
+
+export interface InvoiceItem {
+  id: string;
+  description: string;
+  amount: number;
+}
+
+export interface Invoice {
+  id: string;
+  client: string;
+  year: number;
+  amount: number;
+  status: InvoiceStatus;
+  dueDate: string; // ISO
+  invoiceDate: string; // ISO
+  items: InvoiceItem[];
+}
