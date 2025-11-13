3f39a875fd27a2fd3528a739b1ba2cd42832df84
Łukasz Godlewski <lgodlewski@itsg.com.pl>
2025-11-13 12:46:13 +0100

    fix: typescript issue

--- src/features/sales/PaymentSummary/PaymentSummary.vue
+++ src/features/sales/PaymentSummary/PaymentSummary.vue
@@ -2,15 +2,27 @@
 import { usePaymentSummary } from '../composables/usePaymentSummary';
 import PaymentSummaryView from './PaymentSummaryView.vue';
 import StateWrapper from '../StateWrapper.vue';
+import { computed } from 'vue';
 
 const { paymentSummary, isLoading, error } = usePaymentSummary();
+
+const errorMessage = computed(() => {
+  if (error.value) {
+    return 'The payment summary cannot be fetched';
+  }
+  return null;
+});
 </script>
 
 <template>
-  <StateWrapper :is-loading="isLoading" :error="error" :resource="paymentSummary" class="p-6">
+  <StateWrapper
+    :is-loading="isLoading"
+    :error="errorMessage"
+    :resource="paymentSummary"
+    class="p-6"
+  >
     <template #default="{ resource }">
       <PaymentSummaryView :payment-summary="resource" />
     </template>
-    <template #error> The payment summary cannot be fetched </template>
   </StateWrapper>
 </template>
